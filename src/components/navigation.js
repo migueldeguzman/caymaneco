d38d3dc9c3bba20db77d23b8724241074a4a35a4
migueldeguzmandev <migueldeguzmandev@gmail.com>
2020-01-25 15:55:04 -0500

    adding local build files

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,56 @@ import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
+  <div className={styles.navbar}>
+  <Link to="/">Home</Link>
+  <Link to="/blog">Climate Change Corner</Link>
+  <div className={styles.dropdown}>
+    <button className={styles.dropbtn}>More
+      <i className="fa fa-caret-down"></i>
+    </button>
+    <div className={styles.dropdownContent}>
+      <nav role="navigation">
+        <ul className={styles.navigation}>
+          <li className={styles.navigationItem}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.navigationItem}>
+            <Link to="/blog/">Climate Change Corner</Link>
+          </li>
+          <li className={styles.navigationItem}>
+            <Link to="/blog/">Beyond Cayman</Link>
+          </li>
+          <li className={styles.navigationItem}>
+            <Link to="/blog/">Climate Science 101</Link>
+          </li>
+          <li className={styles.navigationItem}>
+            <Link to="/blog/">Video Eco</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</div>
+
 )
+
+
+  // <nav role="navigation">
+  //   <ul className={styles.navigation}>
+  //     <li className={styles.navigationItem}>
+  //       <Link to="/">Home</Link>
+  //     </li>
+  //     <li className={styles.navigationItem}>
+  //       <Link to="/blog/">Climate Change Corner</Link>
+  //     </li>
+  //     <li className={styles.navigationItem}>
+  //       <Link to="/blog/">Beyond Cayman</Link>
+  //     </li>
+  //     <li className={styles.navigationItem}>
+  //       <Link to="/blog/">Climate Science 101</Link>
+  //     </li>
+  //     <li className={styles.navigationItem}>
+  //       <Link to="/blog/">Video Eco</Link>
+  //     </li>
+  //   </ul>
+  // </nav>
